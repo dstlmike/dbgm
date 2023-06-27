0656a01b47b0cd258a0f00d2c8a73f45de603807
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-27 00:04:21 -0400

    Update rooms.js

--- modules/rooms.js
+++ modules/rooms.js
@@ -45,9 +45,9 @@ exports.getRoom = function(path) {
 //  if (rooms[path]) {
     room.type = path;
     room.id = botID; //rooms[path];
-  }
+//  }
 
-  return room;
+ // return room;
 }
 
 exports.checkCommands = function(dataHash, callback) {

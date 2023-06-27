805c03fcee446acd7f2b9ce20cbd04eb09cf187f
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-27 00:33:25 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -106,8 +106,8 @@ function connect(callback){
 exports.getAllDocuments = async function(collection, callback) {
   mongoDB.connect(connection_string, function(err, db) {
     if(err) throw err;
-    var dbName = db.collection("sampledb").collection("test");
-    var allDocs = db.collection(collection).find().toArray(function(err, docs) {
+    var dbName = "sampledb"; // db.collection("sampledb").collection("test");
+    var allDocs = db.dbname.collection(collection).find().toArray(function(err, docs) {
     alldocs; //  callback(docs);
       console.log(allDocs);
     db.close();
